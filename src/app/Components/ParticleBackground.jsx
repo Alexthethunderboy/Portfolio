a9a5b6789e8c8657b9c4e633e820e19b53f4fbcb
Alexthethunderboy <khaylex2.0@gmail.com>
2025-01-08 10:30:53 +0000

    newww

--- src/app/Components/ParticleBackground.jsx
+++ src/app/Components/ParticleBackground.jsx
@@ -108,7 +108,7 @@ const ParticleBackground = () => {
   return (
     <canvas
       ref={canvasRef}
-      className="absolute top-0 left-0 w-full h-full opacity-40"
+      className="absolute top-0 left-0 w-full h-full md:opacity-70 opacity-40"
       style={{ pointerEvents: 'none' }}
     />
   );
